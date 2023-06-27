42b99b33324272942605910813d7e74c74c5ba13
Gene Boggs <gene@ology.net>
2023-06-27 07:42:47 -0700

    Initial commit

---
+++ lib/Synth/Config.pm
@@ -0,0 +1,192 @@
+package Synth::Config;
+
+# ABSTRACT: Synthesizer settings librarian
+
+our $VERSION = '0.0001';
+
+use Moo;
+use strictures 2;
+use Carp qw(croak);
+use Mojo::JSON qw(to_json);
+use Mojo::SQLite ();
+use namespace::clean;
+
+=head1 SYNOPSIS
+
+  use Synth::Config ();
+
+  my $synth = Synth::Config->new(model => 'Moog Matriarch');
+
+  my $result = $synth->make_setting(...);
+
+  $result = $synth->recall_setting(...);
+
+  $result = $synth->render_setting(...); # TODO
+
+=head1 DESCRIPTION
+
+C<Synth::Config> provides a way to save and recall synthesizer control
+settings in a database.
+
+This does B<not> control the synth. It is simply a way to manually
+record the parameters defined by knob, slider, switch, or patch
+settings in an SQLite database. It is a "librarian", if you will.
+
+=head1 ATTRIBUTES
+
+=head2 model
+
+$model = $synth->model;
+
+Show progress.
+
+=cut
+
+has model => (
+  is       => 'rw',
+  required => 1,
+);
+
+=head2 dbname
+
+  $dbname = $synth->dbname;
+
+Database name
+
+Default: C<synth-config.db>
+
+=cut
+
+has dbname => (
+  is       => 'ro',
+  required => 1,
+  default  => sub { 'synth-config.db' },
+);
+
+has _sqlite => (
+  is => 'lazy',
+);
+
+sub _build__sqlite {
+  my ($self) = @_;
+  my $sqlite = Mojo::SQLite->new('sqlite:' . $self->dbname);
+  return $sqlite->db;
+}
+
+=head2 verbose
+
+  $verbose = $x->verbose;
+
+Show progress.
+
+=cut
+
+has verbose => (
+  is      => 'ro',
+  isa     => sub { croak "$_[0] is not a boolean" unless $_[0] =~ /^[01]$/ },
+  default => sub { 0 },
+);
+
+=head1 METHODS
+
+=for Pod::Coverage BUILD
+
+=head2 new
+
+  $synth = Synth::Config->new(%args);
+
+Create a new C<Synth::Config> object.
+
+=cut
+
+sub BUILD {
+  my ($self, $args) = @_;
+  # sanitize the model name
+  (my $model = $args->{model}) =~ s/\W/_/g;
+  $self->model(lc $model);
+  # create the model table unless it's already there
+  $self->_sqlite->query(
+    'create table if not exists '
+    . $self->model
+    . ' (id integer primary key autoincrement, settings text not null)'
+  );
+}
+
+=head2 make_setting
+
+  my $id = $synth->make_setting(%args);
+
+Save a setting with a db "upsert" operation and return the record id.
+
+Example:
+
+  group  parameter control bottom top   value unit is_default
+  filter cutoff    knob    20     20000 200   Hz   true
+
+=cut
+
+sub make_setting {
+  my ($self, %args) = @_;
+  my $id = delete $args{id};
+  croak 'No columns given' unless keys %args;
+  if ($id) {
+    my $setting = $self->_sqlite->select(
+      $self->model,
+      ['settings'],
+      { id => $id },
+    )->expand(json => 'settings')->hash;
+    my $params = { %{ $setting->{settings} }, %args };
+    $self->_sqlite->update(
+      $self->model,
+      { settings => to_json($params) },
+      { id => $id },
+    );
+  }
+  else {
+    $id = $self->_sqlite->insert(
+      $self->model,
+      {
+        id       => $id,
+        settings => to_json(\%args),
+      },
+    )->last_insert_id;
+  }
+  return $id;
+}
+
+=head2 recall_setting
+
+  my $setting = $synth->recall_setting(%args);
+
+Return the parameters of a setting.
+
+=cut
+
+sub recall_setting {
+  my ($self, %args) = @_;
+  my $id = delete $args{id};
+  croak 'No id given' unless $id;
+  my $setting = $self->_sqlite->select(
+    $self->model,
+    ['settings'],
+    {
+      id => $id,
+    },
+  )->expand(json => 'settings')->hash;
+  return $setting;
+}
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+L<Moo>
+
+Knob: L<https://codepen.io/jhnsnc/pen/KXYayG>
+
+Switch: L<https://codepen.io/magnus16/pen/grzqMz>
+
+Slider: L<?>
+
+=cut
---
+++ t/01-methods.t
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Test::More;
+
+use_ok 'Synth::Config';
+
+my $obj = new_ok 'Synth::Config' => [
+  model   => 'Moog Matriarch',
+  dbname  => 'test.db',
+  verbose => 1,
+];
+
+subtest defaults => sub {
+  is $obj->model, 'moog_matriarch', 'model';
+  is $obj->verbose, 1, 'verbose';
+};
+
+subtest settings => sub {
+  my $expect = {
+    group      => 'filter',
+    parameter  => 'cutoff',
+    control    => 'knob',
+    bottom     => 20,
+    top        => 20_000,
+    value      => 200,
+    unit       => 'Hz',
+    is_default => 0,
+  };
+  # make an initial setting
+  my $id = $obj->make_setting(%$expect);
+  ok $id, "id: $id";
+  # recall that setting
+  my $setting = $obj->recall_setting(id => $id);
+  is_deeply $setting, { settings => $expect }, 'settings';
+  # update a single field in the setting
+  my $got = $obj->make_setting(id => $id, is_default => 1);
+  is $got, $id, 'updated setting';
+  # recall that same setting
+  $setting = $obj->recall_setting(id => $id);
+  is keys(%{ $setting->{settings} }), keys(%$expect), 'settings all there';
+  # check the updated field
+  ok $setting->{settings}{is_default}, 'is_default';
+  # another!
+  $expect = {
+    group      => 'modulation',
+    parameter  => 'wave out',
+    control    => 'patch',
+    bottom     => 0,
+    top        => 1,
+    value      => 1,
+    unit       => 'boolean',
+    is_default => 0,
+  };
+  # make a second setting
+  my $id2 = $obj->make_setting(%$expect);
+  is $id2, $id + 1, "id: $id2";
+  # recall that setting
+  $setting = $obj->recall_setting(id => $id2);
+  is_deeply $setting, { settings => $expect }, 'settings';
+  # update a single field in the setting
+  my $got2 = $obj->make_setting(id => $id2, is_default => 1);
+  is $got2, $id + 1, 'updated setting';
+  # recall that same setting
+  $setting = $obj->recall_setting(id => $got2);
+  is keys(%{ $setting->{settings} }), keys(%$expect), 'settings all there';
+  # check the updated field
+  ok $setting->{settings}{is_default}, 'is_default';
+};
+
+subtest cleanup => sub {
+  ok -e 'test.db', 'db exists';
+  unlink 'test.db';
+  unlink 'test.db-shm';
+  unlink 'test.db-wal';
+  ok !-e 'test.db', 'db unlinked';
+};
+
+done_testing();
