c0b302d60aed0849e959a87a0b06d05b8b7cbd4c
Gene Boggs <gene@ology.net>
2023-06-29 00:00:00 -0700

    Initial commit

---
+++ eg/mojo-ui.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite -signatures;
+
+use Data::Dumper::Compact qw(ddc);
+use Getopt::Long qw(GetOptions);
+use Pod::Usage qw(pod2usage);
+use IO::Prompt::Tiny qw(prompt);
+use Term::Choose ();
+
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(Synth-Config);
+use Synth::Config ();
+
+get '/' => sub ($c) {
+  my $model = $c->param('model') || 'Moog Matriarch';
+  my $group = $c->param('group') || 'arp_seq';
+  my $synth = Synth::Config->new(model => $model);
+  # get a specs config file for the synth model
+  my $set = './eg/' . $synth->model . '.set';
+  my $specs = -e $set ? do $set : undef;
+  my $groups = $specs ? $specs->{group} : undef;
+  $groups = [ sort @$groups ] if $groups;
+  $c->render(
+    template => 'index',
+    model    => $model,
+    group    => $group,
+    groups   => $groups,
+  );
+} => 'index';
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Synth::Config';
+<form action="<%= url_for('index') %>" method="get">
+  <label for="model">Model:</label>
+  <input name="model" id="model" value="<%= $model %>">
+  <label for="group">Modual:</label>
+  <select name="group" id="group">
+% for my $g (@$groups) {
+    <option value="<%= $g %>" <%= $g eq $group ? 'selected' : '' %>><%= ucfirst $g %></option>
+% }
+  </select>
+</form>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
