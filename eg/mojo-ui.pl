4603f8ba66cc0cb747693b6ebb9c6d5dd63b044d
Gene Boggs <gene@ology.net>
2023-06-29 16:59:28 -0700

    WIP

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -187,9 +187,8 @@ __DATA__
 %     unit       => $setting->{unit},
 %     is_default => $setting->{is_default},
 %   );
-%   my $myname = $setting->{name} || $name;
 <a href="<%= $edit_url %>">Edit</a>
-<b>Name</b>: <%= $myname %>,
+<b>Name</b>: <%= $setting->{name} %>,
 <b>Group</b>: <%= $setting->{group} %>,
 <b>Param</b>: <i><%= $setting->{parameter} %></i> <%= $setting->{control} %> (<%= $setting->{bottom} %>-<%= $setting->{top} %>),
 <b>Value</b>: <%= $setting->{value} %> <%= $setting->{unit} %>
