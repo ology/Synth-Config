fddfcf72d1c571b5d7bc1806d1edebc055c883a8
Gene Boggs <gene@ology.net>
2023-06-29 02:15:17 -0700

    WIP

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -11,16 +11,19 @@ use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(Synth-Config);
 use Synth::Config ();
 
 get '/' => sub ($c) {
-  my $model = $c->param('model') || 'Moog Matriarch';
-  my $group = $c->param('group') || 'arp_seq';
-  my $synth = Synth::Config->new(model => $model);
-  # get a specs config file for the synth model
-  my $set = './eg/' . $synth->model . '.set';
-  my $specs = -e $set ? do $set : undef;
-  # get the known groups if there are specs
-  my $groups = $specs ? $specs->{group} : undef;
-  $groups = [ sort @$groups ] if $groups;
-  my $settings = $synth->search_settings(group => $group);
+  my $model = $c->param('model');
+  my $group = $c->param('group');
+  my ($groups, $settings);
+  if ($model) {
+    my $synth = Synth::Config->new(model => $model);
+    # get a specs config file for the synth model
+    my $set = './eg/' . $synth->model . '.set';
+    my $specs = -e $set ? do $set : undef;
+    # get the known groups if there are specs
+    $groups = $specs ? $specs->{group} : undef;
+    $groups = [ sort @$groups ] if $groups;
+    $settings = $synth->search_settings(group => $group);
+  }
   $c->render(
     template => 'index',
     model    => $model,
@@ -29,6 +32,67 @@ get '/' => sub ($c) {
     settings => $settings,
   );
 } => 'index';
+
+get '/edit' => sub ($c) {
+  my $model      = $c->param('model');
+  my $group      = $c->param('group');
+  my $parameter  = $c->param('parameter');
+  my $control    = $c->param('control');
+  my $group_to   = $c->param('group_to');
+  my $param_to   = $c->param('param_to');
+  my $bottom     = $c->param('bottom');
+  my $top        = $c->param('top');
+  my $value      = $c->param('value');
+  my $unit       = $c->param('unit');
+  my $is_default = $c->param('is_default');
+  my $synth = Synth::Config->new(model => $model);
+  # get a specs config file for the synth model
+  my $set = './eg/' . $synth->model . '.set';
+  my $specs = -e $set ? do $set : undef;
+  unless ($specs) {
+    $c->flash(error => 'No known model');
+    return $c->redirect_to('index');
+  }
+  $c->render(
+    template   => 'edit',
+    specs      => $specs,
+    model      => $model,
+    group      => $group,
+    parameter  => $parameter,
+    control    => $control,
+    group_to   => $group_to,
+    param_to   => $param_to,
+    bottom     => $bottom,
+    top        => $top,
+    value      => $value,
+    unit       => $unit,
+    is_default => $is_default,
+  );
+} => 'edit';
+post '/update' => sub ($c) {
+  my $v = $c->validation;
+  $v->required('model');
+  $v->required('group');
+  $v->required('parameter');
+  $v->required('control');
+  $v->optional('group_to');
+  $v->optional('param_to');
+  $v->optional('bottom');
+  $v->optional('top');
+  $v->optional('value');
+  $v->optional('unit');
+  $v->optional('is_default');
+  if ($v->failed->@*) {
+    $c->flash(error => 'Could not update');
+    return $c->redirect_to('edit');
+  }
+  my $synth = Synth::Config->new(model => $v->param('model'));
+  # get a specs config file for the synth model
+  my $set = './eg/' . $synth->model . '.set';
+  my $specs = -e $set ? do $set : undef;
+  my $settings = $synth->search_settings(group => $v->param('group'));
+  $c->redirect_to('index');
+} => 'update';
 
 app->start;
 __DATA__
@@ -57,6 +121,30 @@ __DATA__
 %   }
 <br>
 % }
+
+@@ edit.html.ep
+% layout 'default';
+% title 'Synth::Config Update';
+<form action="<%= url_for('index') %>" method="get">
+  <label for="model">Model:</label>
+  <input name="model" id="model" value="<%= $model %>">
+  <label for="group">Module:</label>
+  <select name="group" id="group">
+% for my $g (@$groups) {
+    <option value="<%= $g %>" <%= $g eq $group ? 'selected' : '' %>><%= ucfirst $g %></option>
+% }
+  </select>
+  <input type="submit" value="Submit">
+parameter
+control
+group_to
+param_to
+bottom
+top
+value
+unit
+is_default
+</form>
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
