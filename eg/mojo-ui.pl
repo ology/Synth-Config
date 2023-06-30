d128d3b1c5adc59e64da86f0bf3e2399feaa6a72
Gene Boggs <gene@ology.net>
2023-06-30 13:51:57 -0700

    Fontawesome

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -379,6 +379,8 @@ $(document).ready(function() {
     <title><%= title %></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="/css/fontawesome.css" rel="stylesheet">
+    <link href="/css/solid.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.min.js"></script>
     <style>
