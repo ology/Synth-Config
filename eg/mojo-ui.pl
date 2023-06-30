55ee3e050ec8880867c2beecbe6868bfd4129e74
Gene Boggs <gene@ology.net>
2023-06-29 22:06:18 -0700

    Use nums

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -251,11 +251,11 @@ __DATA__
   <input type="text" name="value" id="value" value="<%= $selected->{value} %>">
 %   } elsif ($key eq 'is_default') {
   <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="is_default" id="is_default_false" value="false">
+    <input class="form-check-input" type="radio" name="is_default" id="is_default_false" value="0">
     <label class="form-check-label" for="is_default_false">False</label>
   </div>
   <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="is_default" id="is_default_true" value="true">
+    <input class="form-check-input" type="radio" name="is_default" id="is_default_true" value="1">
     <label class="form-check-label" for="is_default_true">True</label>
   </div>
 %   } else {
