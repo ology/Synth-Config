50d94c9916ee24ed8edc31dd52ec03a654668027
Gene Boggs <gene@ology.net>
2023-06-30 14:15:45 -0700

    Pass group

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -295,7 +295,7 @@ __DATA__
   <p></p>
   <button type="submit" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Submit</button>
   <a href="<%= url_for('remove')->query(id => $id, model => $model, name => $name) %>" class="btn btn-danger" onclick="if(!confirm('Remove setting <%= $id %>?')) return false;"><i class="fa-solid fa-trash-can"></i> Remove</a>
-  <a href="<%= url_for('index')->query(model => $model, name => $name) %>" class="btn btn-warning"><i class="fa-solid fa-xmark"></i> Cancel</a>
+  <a href="<%= url_for('index')->query(model => $model, name => $name, group => $selected->{group}) %>" class="btn btn-warning"><i class="fa-solid fa-xmark"></i> Cancel</a>
 </form>
 
 <script>
