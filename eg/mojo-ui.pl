4f79b38addf39d68715d1ce43a8c247f16cef408
Gene Boggs <gene@ology.net>
2023-07-01 12:26:09 -0700

    WTF

--- eg/mojo-ui.pl
+++ eg/mojo-ui.pl
@@ -320,6 +320,7 @@ __DATA__
     <a href="<%= url_for('model') %>" class="btn btn-success" id="new_model"><i class="fa-solid fa-database"></i> New model</a>
 % if ($model) {
     <a href="<%= url_for('edit_model')->query(model => $model) %>" id="edit_model" class="btn btn-success"><i class="fa-solid fa-pencil"></i> Edit model</a>
+
     <a href="<%= url_for('remove')->query(model => $model) %>" id="remove_model" class="btn btn-danger" onclick="if(!confirm('Remove model?')) return false;"><i class="fa-solid fa-trash-can"></i> Remove model</a>
 % }
   </div>
