7b6ac73457e1fa04ff998bc5c5f684008152608a
Gene Boggs <gene@ology.net>
2024-04-08 09:07:50 -0700

    WIP

--- lib/Synth/Config.pm
+++ lib/Synth/Config.pm
@@ -283,32 +283,6 @@ sub search_settings {
   my $results = $self->_sqlite->query($sql);
   my @settings;
   while (my $next = $results->hash) {
-    push @settings, { $next->{id} => from_json($next->{settings}) };
-    # add the setting name to the settings data so we can use it in the templates
-    $settings[-1]->{ $next->{id} }{name} = $next->{name};
-  }
-  return \@settings;
-}
-
-sub _search_settings {
-  my ($self, %args) = @_;
-  my $name = delete $args{name};
-  my @where;
-  push @where, "name = '$name'" if $name;
-  for my $arg (keys %args) {
-    next unless $args{$arg};
-    $args{$arg} =~ s/'/''/g; # escape the single-quote
-    push @where, q/json_extract(settings, '$./ . $arg . q/') = / . "'$args{$arg}'";
-  }
-  return [] unless @where;
-  my $sql = q/select id,name,settings,json_extract(settings, '$.group') as mygroup, json_extract(settings, '$.parameter') as parameter from /
-    . $self->model
-    . ' where ' . join(' and ', @where)
-    . ' order by name,mygroup,parameter';
-  print "Search SQL: $sql\n" if $self->verbose;
-  my $results = $self->_sqlite->query($sql);
-  my @settings;
-  while (my $next = $results->hash) {
     my $set = from_json($next->{settings});
     $set->{id} = $next->{id};
     $set->{name} = $next->{name};
