887108c96d37c532e56a5dc176beb87ad7d5720f
Gene Boggs <gene@ology.net>
2024-05-19 08:40:33 -0700

    Yes

--- eg/patch-visualizer.pl
+++ eg/patch-visualizer.pl
@@ -3,13 +3,8 @@ use strict;
 use warnings;
 
 use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(Synth-Config); # local author libs
-
 use Synth::Config ();
-
-use Data::Dumper::Compact qw(ddc);
 use Getopt::Long qw(GetOptions);
-use List::Util qw(first);
-use YAML qw(LoadFile);
 
 my %opt = ( # defaults:
     model     => undef, # e.g. 'Modular'
@@ -29,32 +24,20 @@ die "Usage: perl $0 --model='Modular' [--patch='Simple 001']\n"
 
 $opt{config} ||= "eg/$model_name.yaml";
 die "Invalid model config\n" unless -e $opt{config};
-my $config = LoadFile($opt{config});
 
 my $synth = Synth::Config->new(model => $model_name);
 
-for my $patch ($config->{patches}->@*) {
-    my $patch_name = $patch->{patch};
+my $patches = $synth->import_yaml(
+    file  => $opt{config},
+    patch => $opt{patch},
+);
 
-    next if $opt{patch} && $patch_name ne $opt{patch};
-
-    my $settings = $synth->search_settings(name => $patch_name);
-
-    if ($settings && @$settings) {
-        print "Removing $patch_name setting from $model_name\n";
-        $synth->remove_settings(name => $patch_name);
-    }
-
-    for my $setting ($patch->{settings}->@*) {
-        print "Adding $patch_name setting to $model_name\n";
-        $synth->make_setting(name => $patch_name, %$setting);
-    }
-    $settings = $synth->search_settings(name => $patch_name);
-
+for my $patch (@$patches) {
+    my $settings = $synth->search_settings(name => $patch);
     my $g = $synth->graphviz(
-      settings   => $settings,
-      model_name => $model_name,
-      patch_name => $patch_name,
-      render     => 1,
+        settings   => $settings,
+        model_name => $model_name,
+        patch_name => $patch,
+        render     => 1,
     );
 }
