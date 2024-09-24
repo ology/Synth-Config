7e00cf7e555e1dcbb09c6e4b26998c87bbbe9893
Gene Boggs <gene@ology.net>
2024-09-24 12:43:49 -0700

    Fix ref

--- eg/patch-visualize.pl
+++ eg/patch-visualize.pl
@@ -24,7 +24,7 @@ my $synth = Synth::Config->new(
 #    verbose => 1,
 );
 
-my $patches = $opt{patch} ? $opt{patch}->@* : $synth->recall_setting_names;
+my $patches = $opt{patch} ? [ $opt{patch}->@* ] : $synth->recall_setting_names;
 
 for my $patch_name (@$patches) {
     my $settings = $synth->search_settings(name => $patch_name);
